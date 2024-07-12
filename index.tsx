b4d507648a6378053b68461bdfdeaead4a9b23b8
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-07-11 20:55:49 -0500

    added htmx script

--- index.tsx
+++ index.tsx
@@ -21,6 +21,7 @@ const BaseHtml = ({ children }: elements.Children) => `
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Elysia</title>
+    <script src="https://unpkg.com/htmx.org@2.0.0"></script>
 </head>
 ${children}
 `
