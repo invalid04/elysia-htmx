a07b3aedb15eae67f15a6d5128765eaceb0b4fa8
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-07-11 21:03:12 -0500

    added tailwind

--- index.tsx
+++ index.tsx
@@ -29,6 +29,7 @@ const BaseHtml = ({ children }: elements.Children) => `
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Elysia</title>
     <script src="https://unpkg.com/htmx.org@2.0.0"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 ${children}
 `
