47e6b32d5933f924e2ac0acb1a45822d6988b9a6
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-07-11 20:45:16 -0500

    serving html

--- index.ts
+++ index.ts
@@ -1,5 +1,18 @@
 import { Elysia } from 'elysia'
+import { html } from '@elysiajs/html'
 
-const app = new Elysia()
+const app = new Elysia().use(html())
 
-.get('/', () => 'hello world').listen(3000)
+.get('/', ({ html }) => html(baseHtml)).listen(3000)
+
+const baseHtml = `
+<DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Elysia</title>
+</head>
+<body>HTML doc</body>
+`
